e482e27ba1b013cb782aa06a91a670e7a956bea9
ms471841 <ms471841@.com>
2021-06-14 22:37:06 +0530

    changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
-import 'package:avatar_glow/avatar_glow.dart';
 import 'package:flutter/material.dart';
+import 'package:glow_avatar/homeScreen.dart';
 
 void main() {
   runApp(Avatarglow());
@@ -12,39 +12,7 @@ class Avatarglow extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text("Geeks for Geeks"),
-          centerTitle: true,
-          backgroundColor: Colors.green,
-        ),
-        body: Center(
-          child: AvatarGlow(
-            glowColor: Colors.green,
-            endRadius: 90.0,
-            duration: Duration(milliseconds: 2000),
-            repeat: true,
-            showTwoGlows: true,
-            repeatPauseDuration: Duration(milliseconds: 100),
-            child: Material(
-              // Replace this child with your own
-              elevation: 8.0,
-              shape: CircleBorder(),
-              child: ClipRRect(
-                borderRadius: BorderRadius.circular(40),
-                child: CircleAvatar(
-                  backgroundColor: Colors.grey[100],
-                  child: Image.network(
-                    "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fmedia.geeksforgeeks.org%2Fwp-content%2Fcdn-uploads%2Fgfg_200X200.png&f=1&nofb=1",
-                    fit: BoxFit.fill,
-                  ),
-                  radius: 40.0,
-                ),
-              ),
-            ),
-          ),
-        ),
-      ),
+      home: HomeScreen(),
     );
   }
 }
